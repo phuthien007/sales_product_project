d35ff5c1ad1bda8ef923dd2334d5ce06730808ce
= <=>
2021-04-28 18:50:00 +0700

    setup giao dien

--- SalesProduct.cpp
+++ SalesProduct.cpp
@@ -2,15 +2,28 @@
 #include <string>
 #include <ctime>
 #include <cstdlib>
+#include<iomanip>
 using namespace std;
-// define struct product
-typedef struct product
-{
-	string name_product;
-	float price;
-} Product;
+time_t t = time(0);
+tm *now = localtime(&t);
+string day, month,year;
+void format_date(){    
+    if(now->tm_mday < 10)
+    {
+        day= "0";
+        day.append(to_string(now->tm_mday));
+    }
+    else day= to_string(now->tm_mday);
+    if(now->tm_mon +1 < 10)
+    {
+        month= "0" ;
+        month.append( to_string(now->tm_mon +1));
+    }
+    else month = to_string(now->tm_mon);
+	year = now->tm_year %100;
+}
 // task1
-void task1(string name_sales, int day, int month, int year)
+void task1(string name_sales)
 {
 	double subtotal = 0, tax = 0, total = 0, balance = 0, amount_paid = 0;
 	double price_A = 11.99, price_B = 12.99, price_C = 14.99, price_D = 15.99;
@@ -19,8 +32,9 @@ void task1(string name_sales, int day, int month, int year)
 	{
 		system("cls");
 		cout << "SALE PRODUCTS CS - " << name_sales << endl;
-		cout << "----------------------------------------------------------------------\n";
-		cout << "\tToday: " << (day < 10 ? '0' + (day + '0') : day) << "/" << (month < 10 ? '0' + (month + '0') : month) << "/" << year % 100 << endl;
+		cout<<setfill('.');
+		cout << setw(50)<<'.'<<endl;
+		cout << "\tToday: " << day << "/" << 	month << "/" << year << endl;
 		cout << "\t 1.\tModel CS21A " << endl;
 		cout << "\t 2.\tModel CS21B " << endl;
 		cout << "\t 3.\tModel CS21C " << endl;
@@ -44,23 +58,27 @@ void task1(string name_sales, int day, int month, int year)
 			total = subtotal + tax;
 			balance = amount_paid - total;
 			// print receipt
-			cout << "............................................." << endl;
+			cout<<setfill('.');
+			cout << setw(50)<<'.' << endl;
 			cout << "RECEIPT - COMPANY CS" << endl;
-			cout << "............................................." << endl;
-			cout << "Date:                                "<< (day < 10 ? '0' + (day + '0') : day) << "/" << (month < 10 ? '0' + (month + '0') : month) << "/" << year % 100 << endl;
-			cout << "Sale transaction:                    03010001" << endl;
-			cout << "Sale employee:                      " <<name_sales<< endl;
-			cout << "............................................." << endl;
-			cout << "Model CS21A (" << price_A << "/per unit)     " << 10 << "    119.90" << endl;
-			cout << "Model CS21B (" << price_B << "/per unit)     " << 10 << "    129.90" << endl;
-			cout << "Model CS21C (" << price_C << "/per unit)     " << 10 << "    149.90" << endl;
-			cout << "Model CS21D (" << price_D << "/per unit)     " << 10 << "    159.90" << endl;
-			cout << "............................................." << endl;
-			cout << "Subtotal:                             "<<subtotal<<"" << endl;
-			cout << "Tax(8.25%)                             "<<tax<< endl;
-			cout << "Total:                                 "<<total << endl;
-			cout << "Amount paid:                           "<<amount_paid<< endl;
-			cout << "Balance:                                "<<balance << endl;
+			cout<<setfill('.');
+			cout << setw(50)<<'.' << endl;
+			cout << "Date:"<<setw(40)<<left<< day << "/" << month << "/" << year << endl;
+			cout << "Sale transaction:"<<setw(40)<<left<<"03010001" << endl;
+			cout << "Sale employee:"<<setw(40) <<left<<name_sales<< endl;
+			cout<<setfill('.');
+			cout << setw(50)<<'.' << endl;
+			cout << "Model CS21A (" << price_A << "/per unit)     " << setw(10)<<left<<qty_A << setw(10)<<right<<price_A*qty_A << endl;
+			cout << "Model CS21B (" << price_B << "/per unit)     " << setw(10)<<left<<qty_B << setw(10)<<right<<price_B*qty_B << endl;
+			cout << "Model CS21C (" << price_C << "/per unit)     " << setw(10)<<left<<qty_C << setw(10)<<right<<price_C*qty_C << endl;
+			cout << "Model CS21D (" << price_D << "/per unit)     " << setw(10)<<left<<qty_D << setw(10)<<right<<price_D*qty_D << endl;
+			cout<<setfill('.');
+			cout << setw(50)<<'.' << endl;
+			cout << "Subtotal: "<<setw(40)<<left<<subtotal << endl;
+			cout << "Tax(8.25%) "<<setw(40)<<left<<subtotal<<tax<< endl;
+			cout << "Total:"<<setw(40)<<left<<subtotal<<total << endl;
+			cout << "Amount paid: "<<setw(40)<<left<<subtotal<<amount_paid<< endl;
+			cout << "Balance: "<<setw(40)<<left<<subtotal<<balance << endl;
 			system("pause");
 			break;
 		}
@@ -101,17 +119,13 @@ void menu()
 	string name_sales;
 	cout << "Enter your name: ";
 	getline(cin, name_sales);
-	time_t now = time(0);
-	tm *ltm = localtime(&now);
-	int day = ltm->tm_mday;
-	int month = (1 + ltm->tm_mon);
-	int year = 1990 + ltm->tm_year;
+	
 	while (1)
 	{
 		system("cls");
 		cout << "SALE PRODUCTS CS - " << name_sales << endl;
 		cout << "----------------------------------------------------------------------\n";
-		cout << "\tToday: " << (day < 10 ? '0' + (day + '0') : day) << "/" << (month < 10 ? '0' + (month + '0') : month) << "/" << year % 100 << endl;
+		cout << "\tToday: " << day << "/" << month << "/" << year << endl;
 		cout << "\t\t1.\tSale Product CS " << endl;
 		cout << "\t\t2.\tEnding day sale report  " << endl;
 		cout << "\t\t3.\tEnding month sale report " << endl;
@@ -129,7 +143,7 @@ void menu()
 		case 1:
 		{
 			// task 1
-			task1(name_sales, day, month, year);
+			task1(name_sales);
 			break;
 		}
 		case 2:
@@ -155,6 +169,7 @@ void menu()
 }
 int main()
 {
+	format_date();
 	menu();
 	return 0;
 }
